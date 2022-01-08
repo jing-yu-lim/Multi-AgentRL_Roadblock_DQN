d3d9926e5ff56765a40d00da4b6f27076db6cad1
Jay Lim <jing_yu_lim@outlook.com>
2022-01-09 01:29:46 +0800

    added random seed

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from environment import Roadblock_Env
 import numpy as np
 import os
 if __name__ == '__main__':
-    env = Roadblock_Env()
+    seed_value = 999
+    np.random.seed(seed_value)
+    env = Roadblock_Env(left_type=0, right_type = 1)
     left_agent = Agent(gamma = 0.99, epsilon = 1.0, batch_size = 64, n_actions=2, eps_min=0.01, input_dims=[4],lr = 0.003)
     right_agent = Agent(gamma = 0.99, epsilon = 1.0, batch_size = 64, n_actions=2, eps_min=0.01, input_dims=[4],lr = 0.003)
     
     left_scores, right_scores, total_scores, eps_history = [], [], [], [] 
-    n_games =1000
+    n_games =3000
     target_update=50 #how often to update target network
     lr_step=50       #how often to step through scheduler
 
@@ -29,7 +31,6 @@ if __name__ == '__main__':
             # print(f'\nleft action: {left_action} | rightaction: {right_action}')
             # print(f'left reward: {left_reward} | right reward: {right_reward}')
             # print(f"old obs: {observation} | new obs: {observation_} | done: {done}\n")
-
 
             left_agent.store_transition(np.copy(observation), left_action, left_reward, np.copy(observation_), done)
             right_agent.store_transition(np.copy(observation), right_action, right_reward, np.copy(observation_), done)
@@ -59,14 +60,14 @@ if __name__ == '__main__':
         print('episode',i, 'left score %.2f' % left_score,'right score %.2f' % right_score, 'average score %.2f' % avg_total_score, 'epsilon %.2f' % left_agent.epsilon)
     
     x=[i+1 for i in range(n_games)]
-    dir_name = './results_diagram/3000/'
+    dir_name = './results_diagram/MDP_3000/'
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
 
-    filename = dir_name+'MPD_Roadblock_left.png'
+    filename = dir_name+'MDP_Roadblock_Left.png'
     plotLearning(x, left_scores, eps_history, filename)
-    filename = dir_name+'MPD_Roadblock_right.png'
+    filename = dir_name+'MDP_Roadblock_Right.png'
     plotLearning(x, right_scores, eps_history, filename)
-    filename = dir_name+'MPD_Roadblock_total.png'
+    filename = dir_name+'MDP_Roadblock_Total.png'
     plotLearning(x, total_scores, eps_history, filename)
     
